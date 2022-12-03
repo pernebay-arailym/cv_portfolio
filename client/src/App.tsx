b8bc20729f628c32269144b79e6b4dba3b15c545
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-03 20:34:28 +0100

    :construction: Home, Footer imported

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
 import "./App.css";
+import Footer from "./PortfolioContainer/Home/Footer/Footer";
+import Home from "./PortfolioContainer/Home/Home";
 import Profile from "./PortfolioContainer/Home/Profile";
 
 const App = () => {
   return (
     <div className="App">
       <Profile />
+      <Home />
+      <Footer />
     </div>
   );
 };
