8ca54f97544f6afd7bb0b2efbde269ede0664cf0
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-04 20:39:48 +0100

    :recycle: App updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
-// import Home from "./PortfolioContainer/Home/Home";
 
 const App = () => {
   return (
     <div className="App">
+      <ToastContainer />
       <PortfolioContainer />
     </div>
   );
