e19d1f14d1fadebcdff351945e98d50093527fa4
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-04 21:41:14 +0100

    :construction: refactor utils

--- client/src/utilities/commonUtils.tsx
+++ client/src/utilities/commonUtils.tsx
@@ -1,10 +1,30 @@
+import AboutMe from "../PortfolioContainer/AboutMe/AboutMe";
+import ContactMe from "../PortfolioContainer/ContactMe/ContactMe";
 import Home from "../PortfolioContainer/Home/Home";
+import Resume from "../PortfolioContainer/Resume/Resume";
+import Testimonial from "../PortfolioContainer/Testimonial/Testimonial";
 
 export const TOTAL_SCREENS = [
   {
     screen_name: "Home",
     component: Home,
     alreadyRendered: false,
+  },
+  {
+    screen_name: "AboutMe",
+    component: AboutMe,
+  },
+  {
+    screen_name: "Resume",
+    component: Resume,
+  },
+  {
+    screen_name: "Testimonial",
+    component: Testimonial,
+  },
+  {
+    screen_name: "ContactMe",
+    component: ContactMe,
   },
 ];
 export const GET_SCREEN_INDEX = (screen_name: string) => {
