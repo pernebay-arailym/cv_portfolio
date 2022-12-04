1101d1f4e6da5756eb5ac077dc2b0f3897e88b49
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-04 20:38:40 +0100

    :construction: Footer created

---
+++ client/src/PortfolioContainer/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import ScrollService from "../../utilities/ScrollService";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="scroll-container">
+      <button
+        className="btn-scroll"
+        onClick={() => ScrollService.scrollHandler.scrollToHome()}
+      >
+        {" "}
+        <i className="fa fa-arrow-up"></i>
+      </button>
+    </div>
+  );
+}
