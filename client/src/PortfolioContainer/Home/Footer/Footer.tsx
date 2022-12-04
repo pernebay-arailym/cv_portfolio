e1fa71b2921cd1a7e467ebce375939ff7d1ca52a
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-04 17:20:23 +0100

    :lipstick: Footer

--- client/src/PortfolioContainer/Home/Footer/Footer.tsx
+++ client/src/PortfolioContainer/Home/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer-container">
+      <div className="footer-parent">
         <img
           src={require("../../../assets/Home/shape-bg.png")}
           alt="not internet connection"
