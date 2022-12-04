33790a182990dd63b6a93d901771f597a7e68c1a
chicoumbetov <shyngysmaaan@gmail.com>
2022-12-04 21:56:04 +0100

    :bug: Header import

--- client/src/PortfolioContainer/Home/Home.tsx
+++ client/src/PortfolioContainer/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 import "./Home.css";
 import Profile from "./Profile/Profile";
 
 const Home = () => {
   return (
     <div className="home-container">
+      <Header />
       <Profile />
       <Footer />
     </div>
