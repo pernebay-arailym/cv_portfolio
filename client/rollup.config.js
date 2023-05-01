cc7cc0e732ace22970ddc7fd65be96d82bb3420a
chicoumbetov <shyngysmaaan@gmail.com>
2023-05-01 20:27:22 +0200

    rollup added

---
+++ client/rollup.config.js
@@ -0,0 +1,16 @@
+// rollup.config.js
+
+import { terser } from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "cjs",
+  },
+  plugins: [
+    terser({
+      mangle: false,
+    }),
+  ],
+};
